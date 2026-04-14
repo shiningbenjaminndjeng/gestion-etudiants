6dc62e642e95c0e27e2c7705f88727bbdd6808f9
SHINING <benjaminndjengshining@gmail.com>
2026-04-14 13:54:19 +0100

    Reformat wsgi.py for consistent import style

--- gestion_etudiants/wsgi.py
+++ gestion_etudiants/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion-etudiants')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_etudiants')
 
 application = get_wsgi_application()
