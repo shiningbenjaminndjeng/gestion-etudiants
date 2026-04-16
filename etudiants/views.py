df9598b389ee94cbb33b3b2a6cbb8d81955ac23f
SHINING <benjaminndjengshining@gmail.com>
2026-04-16 11:07:30 +0100

    Update views.py

--- etudiants/views.py
+++ etudiants/views.py
@@ -12,23 +12,23 @@ class UniteEnseignementViewSet(viewsets.ModelViewSet):
     filter_backends  = [filters.SearchFilter]
     search_fields    = ['nom_unite', 'nom_prof']
 
-    class EtudiantViewSet(viewsets.ModelViewSet):
-        queryset         = Etudiant.objects.select_related('unite').all()
-        serializer_class = EtudiantSerializer
-        filter_backends  = [filters.SearchFilter, filters.OrderingFilter]
-        search_fields    = ['nom', 'prenom', 'ethnie', 'unite__nom_unite']
-        ordering_fields  = ['nom', 'age', 'date_inscription']
-    
-        # Endpoint bonus : /api/etudiants/{id}/fiche/
-        @action(detail=True, methods=['get'])
-        def fiche(self, request, pk=None):
-            etudiant = self.get_object()
-            data = {
-                'nom_complet': f"{etudiant.prenom} {etudiant.nom}",
-                'age': etudiant.age,
-                'naissance': f"{etudiant.date_naissance} à {etudiant.lieu_naissance}",
-                'ethnie': etudiant.ethnie,
-                'unite': etudiant.unite.nom_unite if etudiant.unite else None,
-                'professeur': etudiant.unite.nom_prof if etudiant.unite else None,
-            }
-            return Response(data)
+
+class EtudiantViewSet(viewsets.ModelViewSet):
+    queryset         = Etudiant.objects.select_related('unite').all()
+    serializer_class = EtudiantSerializer
+    filter_backends  = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields    = ['nom', 'prenom', 'ethnie', 'unite__nom_unite']
+    ordering_fields  = ['nom', 'age', 'date_inscription']
+
+    @action(detail=True, methods=['get'])
+    def fiche(self, request, pk=None):
+        etudiant = self.get_object()
+        data = {
+            'nom_complet': f"{etudiant.prenom} {etudiant.nom}",
+            'age': etudiant.age,
+            'naissance': f"{etudiant.date_naissance} à {etudiant.lieu_naissance}",
+            'ethnie': etudiant.ethnie,
+            'unite': etudiant.unite.nom_unite if etudiant.unite else None,
+            'professeur': etudiant.unite.nom_prof if etudiant.unite else None,
+        }
+        return Response(data)
