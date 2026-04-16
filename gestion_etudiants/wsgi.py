99e394e37fcbb17b0ec56f74991af98d995755c3
SHINING <benjaminndjengshining@gmail.com>
2026-04-16 11:05:07 +0100

    Fix WSGI settings module path

--- gestion_etudiants/wsgi.py
+++ gestion_etudiants/wsgi.py
@@ -3,6 +3,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_etudiants')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_etudiants.settings')
 
 application = get_wsgi_application()
