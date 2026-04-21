d6184383e1fcdc20a2a46cbe62d6b16a64eb2b69
SHINING <benjaminndjengshining@gmail.com>
2026-04-21 01:02:34 +0100

    Add genre field to etudiant model
    Add 'genre' field to 'etudiant' model with choices for Masculin and Féminin.

---
+++ etudiants/migrations/000X_add_genre_etudiant.py
@@ -0,0 +1,26 @@
+# etudiants/migrations/000X_add_genre_etudiant.py
+# Générez automatiquement via : python manage.py makemigrations
+# Ce fichier est fourni à titre indicatif.
+ 
+from django.db import migrations, models
+ 
+ 
+class Migration(migrations.Migration):
+ 
+    dependencies = [
+        ('etudiants', '0001_initial'),   # ← ajustez selon votre dernière migration
+    ]
+ 
+    operations = [
+        migrations.AddField(
+            model_name='etudiant',
+            name='genre',
+            field=models.CharField(
+                choices=[('M', 'Masculin'), ('F', 'Féminin')],
+                default='M',
+                max_length=1,
+                verbose_name='Genre',
+            ),
+        ),
+    ]
+ 
