f5e75b70038727221ed2ff433bd687ae141254ee
SHINING <benjaminndjengshining@gmail.com>
2026-04-22 18:12:10 +0100

    Refactor EtudiantAdmin display and filtering options

--- etudiants/admin.py
+++ etudiants/admin.py
@@ -4,12 +4,11 @@ from .models import Etudiant, UniteEnseignement
 
 @admin.register(UniteEnseignement)
 class UniteAdmin(admin.ModelAdmin):
-    list_display  = ['nom_unite', 'nom_prof']
+    list_display = ['nom_unite', 'nom_prof']
     search_fields = ['nom_unite', 'nom_prof']
 
 @admin.register(Etudiant)
 class EtudiantAdmin(admin.ModelAdmin):
-    list_display   = ['matricule', 'nom', 'prenom', 'age', 'genre', 'date_inscription']
-    list_filter    = ['genre', 'ethnie', 'unites']
-    search_fields  = ['matricule', 'nom', 'prenom']
-    filter_horizontal = ['unites']
+    list_display  = ['nom', 'prenom', 'age', 'unite', 'date_inscription']
+    list_filter   = ['unite', 'ethnie']
+    search_fields = ['nom', 'prenom']
