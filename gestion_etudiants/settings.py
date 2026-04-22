291b0ab9ef5c0a22b2ff734b6f1a00835359056f
SHINING <benjaminndjengshining@gmail.com>
2026-04-22 11:29:33 +0100

    Update settings.py

--- gestion_etudiants/settings.py
+++ gestion_etudiants/settings.py
@@ -60,14 +60,11 @@ DATABASES = {
     }
 }
 
-# ✅ CORS — autorise TOUT sans restriction
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
-CORS_ALLOW_HEADERS = [
-    'accept', 'accept-encoding', 'authorization',
-    'content-type', 'dnt', 'origin',
-    'user-agent', 'x-csrftoken', 'x-requested-with',
+# ← ajoute tout en bas
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    " https://gestion-etudiants-1-thkp.onrender.com ",  # ← URL de ton frontend Render
 ]
 
 STATIC_URL  = '/static/'
