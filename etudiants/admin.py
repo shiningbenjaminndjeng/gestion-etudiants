0000517e276d37c6e5e46705935221cf084a7e42
SHINING <benjaminndjengshining@gmail.com>
2026-04-23 23:42:23 +0100

    Update Etudiant and UniteEnseignement admin configurations

--- etudiants/admin.py
+++ etudiants/admin.py
@@ -2,13 +2,16 @@
 from django.contrib import admin
 from .models import Etudiant, UniteEnseignement
 
+
 @admin.register(UniteEnseignement)
 class UniteAdmin(admin.ModelAdmin):
-    list_display = ['nom_unite', 'nom_prof']
+    list_display  = ['nom_unite', 'nom_prof']
     search_fields = ['nom_unite', 'nom_prof']
+
 
 @admin.register(Etudiant)
 class EtudiantAdmin(admin.ModelAdmin):
-    list_display  = ['nom', 'prenom', 'age', 'unite', 'date_inscription']
-    list_filter   = ['unite', 'ethnie']
-    search_fields = ['nom', 'prenom']
+    list_display   = ['matricule', 'nom', 'prenom', 'age', 'genre', 'date_inscription']
+    list_filter    = ['genre', 'unites', 'ethnie']
+    search_fields  = ['matricule', 'nom', 'prenom']
+    filter_horizontal = ['unites']   # widget M2M ergonomique dans l'admin
