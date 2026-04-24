66d651f710f2f540e30e8896d775cc954b56d7b2
SHINING <benjaminndjengshining@gmail.com>
2026-04-24 09:45:25 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
- 
+
 pip install -r requirements.txt
- 
+
 python manage.py collectstatic --no-input
- 
-# ✅ Forcer la migration meme si la DB a déjà des tables
-python manage.py migrate --run-syncdb
- 
+
+python manage.py migrate
